6f8744e5a74151dc8859704dc73d389d01d2c5d8
garyhsieh <garyhsieh@gmail.com>
2013-10-18 17:11:06 -0700

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MturkSurvey::Application.config.session_store :cookie_store, key: '_mturk_survey_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MturkSurvey::Application.config.session_store :active_record_store
