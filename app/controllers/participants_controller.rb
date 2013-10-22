4199d99f616da53c49340ad650aeaa89b2301bd0
garyhsieh <garyhsieh@gmail.com>
2013-10-22 14:53:28 -0700

    ready for update

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -44,10 +44,10 @@ class ParticipantsController < ApplicationController
 
     if @participant.save
 
-      ##redirect_to('https://www.mturk.com/mturk/externalSubmit?condition='+@participant.cond.to_s+'&participantId='+@participant.id.to_s+'&assignmentId='+@participant.assignment_id.to_s+'&hitId='+@participant.hit_id.to_s+'&workerId='+@participant.worker_id.to_s+'') 
+      redirect_to('https://www.mturk.com/mturk/externalSubmit?condition='+@participant.cond.to_s+'&participantId='+@participant.id.to_s+'&assignmentId='+@participant.assignment_id.to_s+'&hitId='+@participant.hit_id.to_s+'&workerId='+@participant.worker_id.to_s+'') 
 
 
-      redirect_to('https://workersandbox.mturk.com/mturk/externalSubmit?condition='+@participant.cond.to_s+'&participantId='+@participant.id.to_s+'&assignmentId='+@participant.assignment_id.to_s+'&hitId='+@participant.hit_id.to_s+'&workerId='+@participant.worker_id.to_s+'') 
+      ##redirect_to('https://workersandbox.mturk.com/mturk/externalSubmit?condition='+@participant.cond.to_s+'&participantId='+@participant.id.to_s+'&assignmentId='+@participant.assignment_id.to_s+'&hitId='+@participant.hit_id.to_s+'&workerId='+@participant.worker_id.to_s+'') 
 
     else
       render :action => "new" 
